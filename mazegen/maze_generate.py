57e49ca177c9a4d9477df471375a487757582698
Hikaru Wakatsuki <hwakatsu@c5r4s6.42tokyo.jp>
2026-02-23 15:22:55 +0900

    wak

--- mazegen/maze_generate.py
+++ mazegen/maze_generate.py
@@ -84,16 +84,25 @@ class MazeApp(ABC):
             return False
         return True
 
-    def _add_extra_connection(self) -> None:
+    def _add_extra_connection(self, probability: float = 0.05) -> None:
         """
         不完全迷路用に追加通路を確率的に開通させる
         """
+        is_break: bool = False
         for y in range(self.height):
             for x in range(self.width):
                 for d in (DIRECTIONS["to_east"], DIRECTIONS["to_south"]):
                     if self._can_break_wall(x, y, d):
-                        self._break_wall(x, y, d)
-                        return
+                        if self.rng.random() < probability:
+                            self._break_wall(x, y, d)
+                            is_break = True
+        if not is_break:
+            for y in range(self.height):
+                for x in range(self.width):
+                    for d in (DIRECTIONS["to_east"], DIRECTIONS["to_south"]):
+                        if self._can_break_wall(x, y, d):
+                            self._break_wall(x, y, d)
+                            return
 
     def _is_outer_wall(self, x: int, y: int, d: Direction) -> bool:
         """
